7f76b0fb8c1e085899b2fde1781e800a6e364187
icecream <mansourjohn@gmail.com>
2015-06-09 13:48:32 +1000

    update py path

--- etc/profile.d/underworld.sh
+++ etc/profile.d/underworld.sh
@@ -2,4 +2,4 @@
 
 export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:$LD_LIBRARY_PATH
 export PATH=$PATH:/opt/underworld/libUnderworld/build/bin
-export PYTHONPATH=$PYTHONPATH:/opt/underworld
+export PYTHONPATH=$PYTHONPATH:/home/researcher/underworld:/opt/underworld
